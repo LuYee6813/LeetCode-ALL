43a11bc604b0c5749e9b2c8682e8950a65815b0d
LuYee <aulaul920106@gmail.com>
2022-04-13 23:39:43 +0800

    update

--- #136 Single Number/solution1.c
+++ #136 Single Number/solution1.c
@@ -1,9 +1,4 @@
 int singleNumber(int* nums, int numsSize){
-//     [2,2,1]
-// *nums[0] == *(nums+0) 2
-// *nums[1] == *(nums+1) 2
-// *nums[2] == *(nums+2) 1
-    
     for (int i=0; i<3;i++){
         printf("%d\n",nums[i]);
         int count = 0;
