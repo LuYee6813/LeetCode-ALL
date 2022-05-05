16b749acc9c58228a14d9d722520330c84c737d7
LuYee <aulaul920106@gmail.com>
2022-05-05 17:54:41 +0800

    update

--- #528 Random Pick with Weight/solution.py
+++ #528 Random Pick with Weight/solution.py
@@ -1,13 +1,7 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        
         self.__w = w
-        # 累積圖
-        # a[0] = w[0]
-        # a[1] = a[0] + w[1]
-        # a[2] = a[1] + w[2]
-        
         a = [None] * len(w)
         a[0] = w[0]
         for i in range(1, len(w)):
@@ -15,16 +9,27 @@ class Solution:
         self.__a = a
         print(a)
         
-
     def pickIndex(self) -> int:
         d = random.randrange(self.__a[-1])
-        
         for i, c in enumerate(self.__a):
             if d < c:
                 return i    
         
 
+        # 累積圖概念解題
+        # a[0] = w[0]
+        # a[1] = a[0] + w[1]
+        # a[2] = a[1] + w[2]
 
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(w)
-# param_1 = obj.pickIndex()
+        # 運作邏輯舉例
+        #     0  1  2
+        # w: [1, 3, 3]
+
+        #        1        4       7   <- a : 紀錄變化數字的指標位置
+        #        V        V       V
+        # d:  0  1  2  3  4  5  6     <- d :是從中隨機選一個
+        # l: [0, 1, 1, 1, 2, 2, 2]    <- l :與 w 成正比產生造成需資源太多故用 a 取代
+
+        #     0  1  2  <- i
+        # a: [1, 4, 7] <- c
+
